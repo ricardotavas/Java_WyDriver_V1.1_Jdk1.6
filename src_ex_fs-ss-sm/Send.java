07fb65bf39e917beb2138e66d8325b7547c4ef38
ricardotavas <32338619+ricardotavas@users.noreply.github.com>
2018-03-16 11:54:54 -0300

    Add files via upload

--- src_ex_fs-ss-sm/Send.java
+++ src_ex_fs-ss-sm/Send.java
@@ -20,9 +20,11 @@ public class Send extends JFrame implements ActionListener {
 	static JLabel lbl1= new JLabel("Digite o IP do painel:");
 	static JLabel lbl2=new JLabel("Status de comunicação:");
 	static JLabel lbl3= new JLabel("Número do box:");
+	static JLabel lbl4= new JLabel("Direção da seta:");
 	static JTextField IP= new JTextField("192.168.0.161");
 	static JTextField STATUS= new JTextField("");
 	static JComboBox BOX = new JComboBox ();
+	static JComboBox SETA = new JComboBox ();
 
 	public Send() {
 		super("Exemplo FS-SM-SS sequêncial");
@@ -30,6 +32,10 @@ public class Send extends JFrame implements ActionListener {
 		for (int i=0 ; i<32 ; i++)
 			BOX.addItem(i);
 		BOX.setSelectedItem(3);
+		
+		SETA.addItem("D");
+		SETA.addItem("E");
+		SETA.setSelectedItem("D");		
 
 		btn1.addActionListener(this);
 		btn2.addActionListener(this);
@@ -51,7 +57,12 @@ public class Send extends JFrame implements ActionListener {
 								.addGroup(layout.createParallelGroup(LEADING)
 										.addComponent(lbl3))
 								.addGroup(layout.createParallelGroup(LEADING)
-										.addComponent(BOX)))						
+										.addComponent(BOX)))
+						.addGroup(layout.createSequentialGroup()
+								.addGroup(layout.createParallelGroup(LEADING)
+										.addComponent(lbl4))
+								.addGroup(layout.createParallelGroup(LEADING)
+										.addComponent(SETA)))																
 						.addGroup(layout.createSequentialGroup()
 								.addGroup(layout.createParallelGroup(LEADING)
 										.addComponent(btn1,100,200,300))
@@ -69,7 +80,10 @@ public class Send extends JFrame implements ActionListener {
 						.addComponent(IP))
 				.addGroup(layout.createParallelGroup(BASELINE)
 						.addComponent(lbl3)
-						.addComponent(BOX))					
+						.addComponent(BOX))
+				.addGroup(layout.createParallelGroup(BASELINE)
+						.addComponent(lbl4)
+						.addComponent(SETA))											
 				.addGroup(layout.createParallelGroup(BASELINE)
 						.addComponent(btn1,20,30,40)
 						.addComponent(btn2,20,30,40))
@@ -84,8 +98,12 @@ public class Send extends JFrame implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		Socket sock = new Socket();
 		Packet pack;
+		
+		byte seta='D';
+		if (SETA.getSelectedItem()=="E") seta='E';
+		
 		if (e.getActionCommand()=="Comando Próximo") {
-			pack = new Packet((byte) 255, (byte) 255, 'S', (byte) 51, (byte) 10, (Integer)BOX.getSelectedItem(), 3, new byte[] { 4,1,1 });
+			pack = new Packet((byte) 255, (byte) 255, 'S', (byte) 51, (byte) 10, (Integer)BOX.getSelectedItem(), 3, new byte[] { 4,1,1,seta });
 			sock.Send(IP.getText(), pack.getPacket());
 			if (sock.getStatus()==Const.FRM_OK)
 				STATUS.setText("Comunicação bem sucedida.");
@@ -93,7 +111,7 @@ public class Send extends JFrame implements ActionListener {
 				STATUS.setText("Ocorreu um erro.");			
 		}
 		else if (e.getActionCommand()=="Comando Repete") {
-			pack = new Packet((byte) 255, (byte) 255, 'S', (byte) 51, (byte) 10, (Integer)BOX.getSelectedItem(), 3, new byte[] { 4,1,2 });
+			pack = new Packet((byte) 255, (byte) 255, 'S', (byte) 51, (byte) 10, (Integer)BOX.getSelectedItem(), 3, new byte[] { 4,1,2,seta });
 			sock.Send(IP.getText(), pack.getPacket());
 			if (sock.getStatus()==Const.FRM_OK)
 				STATUS.setText("Comunicação bem sucedida.");
